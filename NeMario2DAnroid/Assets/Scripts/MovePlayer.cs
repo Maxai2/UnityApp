98ac8081a5189d786cad62d920b7b05bf10cfa43
Махмудов Али Ильгар <Mahm_iz81@itstep.edu.az>
2019-09-19 12:10:17 +0400

    'and'

---
+++ NeMario2DAnroid/Assets/Scripts/MovePlayer.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class MovePlayer : MonoBehaviour {
+
+    private Rigidbody2D playerRigid;
+    public float speed = 1f;
+    private Vector2 orgVector;
+    public FixedJoystick joystick;
+
+    //public float jumpHeight = 3f;
+    public static bool isJumping = false;
+
+    void Start() {
+        playerRigid = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate() {
+        float localAxis = 0;
+        float jumpHeight = 0;
+        float axis = joystick.Horizontal;
+        float axisUp = joystick.Vertical;
+
+        if (axis >= .6f) {
+            localAxis = 1;
+        } else if (axis <= -.6f) {
+            localAxis = -1;
+        } else {
+            localAxis = 0;
+        }
+
+        if (axisUp >= .6f && isJumping == false) {
+            jumpHeight = 1.5f;
+        } else {
+            jumpHeight = 0;
+        }
+
+        Vector2 v2 = new Vector2(localAxis, jumpHeight);
+
+        transform.Translate(v2 * speed * Time.fixedDeltaTime);
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision) {
+        if (collision.collider.tag == "Ground") {
+            isJumping = false;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision) {
+        if (collision.collider.tag == "Ground") {
+            isJumping = true;
+        }
+    }
+}
--- NeMarioNew/Assets/Scripts/ScriptPersonMove.cs
+++ NeMarioNew/Assets/Scripts/ScriptPersonMove.cs
@@ -6,11 +6,11 @@ using UnityEngine.Events;
 [RequireComponent(typeof(Rigidbody2D))]
 public class ScriptPersonMove : MonoBehaviour {
 
-    public float speed = 0.3f;
+    public float speed = 5f;
     public float jumpForce = 1;
     private Rigidbody2D rigidbody;
 
-    [SerializeField] private float m_JumpForce = 400f;                          // Amount of force added when the player jumps.
+    [SerializeField] private float m_JumpForce = 10f;                          // Amount of force added when the player jumps.
     [Range(0, 1)] [SerializeField] private float m_CrouchSpeed = .36f;          // Amount of maxSpeed applied to crouching movement. 1 = 100%
     [Range(0, .3f)] [SerializeField] private float m_MovementSmoothing = .05f;  // How much to smooth out the movement
     [SerializeField] private bool m_AirControl = false;                         // Whether or not a player can steer while jumping;
@@ -150,9 +150,9 @@ public class ScriptPersonMove : MonoBehaviour {
         }
 
         if (Input.GetKey(KeyCode.Space)) {
-            Move(speed, false, true);
+            //Move(speed, false, true);
 
-            //rigidbody.AddForce(new Vector2(0f, m_JumpForce), ForceMode2D.Impulse);
+            rigidbody.AddForce(new Vector2(0f, m_JumpForce), ForceMode2D.Impulse);
         }
     }
 }
